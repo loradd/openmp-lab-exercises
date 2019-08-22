a3399a23b1d227c78fe412a085c1781ec6d0be90
Lorenzo Addazi <lorenzo.addazi@mdh.se>
2019-08-22 15:30:15 +0200

    [shwater] parallelized version
    Signed-off-by: Lorenzo Addazi <lorenzo.addazi@mdh.se>

--- advanced_lab/c/shwater2d.c
+++ advanced_lab/c/shwater2d.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-//#include <omp.h>
+#include <omp.h>
 #include <sys/time.h>
 
 #define cell_size 3
@@ -30,10 +30,10 @@ void validate(double *Q, int m, int n) {
   for (i = 0; i < n; i++) 
     for (j = 0; j < m; j++) 
       for (k = 0; k < cell_size;  k++)
-	if (!isfinite(Q(k, j, i))) {
-	  fprintf(stderr, "Invalid solution\n");
-	  exit(-1);
-	}
+  if (!isfinite(Q(k, j, i))) {
+    fprintf(stderr, "Invalid solution\n");
+    exit(-1);
+  }
 }
 
 /* Flux function in the x-direction */
@@ -41,6 +41,7 @@ void fx(double *Q, double **fq, int m, int n, int j) {
   int i;
   const double g = 9.81;
 
+  #pragma omp for
   for (i = 0; i < m; i++) {
     fq[0][i] = Q(1, i, j);
     fq[1][i] = (pow(Q(1, i, j), 2) / Q(0, i, j))  + 
@@ -55,6 +56,7 @@ void fy(double *Q, double **fq, int m, int n, int i) {
   int j;
   const double g= 9.81;
 
+  #pragma omp for
   for (j = 0; j < n; j++) {
     fq[0][j] = Q(2, i, j);
     fq[1][j] = (Q(1, i, j) * Q(2, i, j)) / Q(0, i, j);    
@@ -69,32 +71,40 @@ void fy(double *Q, double **fq, int m, int n, int i) {
   Try to parallelize it in an efficient way!
 */
 void laxf_scheme_2d(double *Q, double **ffx, double **ffy, double **nFx, double **nFy,
-		    int m, int n, double dx, double dy, double dt) {
+        int m, int n, double dx, double dy, double dt) {
   int i, j, k;
-    
+
   /* Calculate and update fluxes in the x-direction */
   for (i = 1; i < n; i++) {
+    #pragma omp single
     fx(Q, ffx, m, n, i);
+    
+    #pragma omp for collapse(2)
     for (j = 1; j < m; j++) 
       for (k = 0; k < cell_size;  k++) 
-	nFx[k][j] = 0.5 * ((ffx[k][j-1] + ffx[k][j]) -
-			   dx/dt * (Q(k, j, i) - Q(k, j-1, i)));
+        nFx[k][j] = 0.5 * ((ffx[k][j-1] + ffx[k][j]) - dx/dt * (Q(k, j, i) - Q(k, j-1, i)));
+    
+    #pragma omp for collapse(2)
     for (j = 1; j < m-1; j++)
       for (k = 0; k < cell_size;  k++) 
-	Q(k, j, i) = Q(k, j, i)  - dt/dx * ((nFx[k][j+1] - nFx[k][j]));
-      
+        Q(k, j, i) = Q(k, j, i)  - dt/dx * ((nFx[k][j+1] - nFx[k][j]));
+  
   }
 
   /* Calculate and update fluxes in the y-direction */
   for (i = 1; i < m; i++) {
+    #pragma omp single
     fy(Q, ffy, m, n, i);
+    
+    #pragma omp for collapse(2)
     for (j = 1; j < n; j++)
       for (k = 0; k < cell_size; k++)
-	nFy[k][j] = 0.5 * ((ffy[k][j-1] + ffy[k][j]) - 
-			   dy/dt * (Q(k, i, j) - Q(k, i, j -1)));
+        nFy[k][j] = 0.5 * ((ffy[k][j-1] + ffy[k][j]) - dy/dt * (Q(k, i, j) - Q(k, i, j -1)));
+    
+    #pragma omp for collapse(2)
     for (j = 1; j <  n-1; j++) 
       for (k = 0; k < cell_size; k++)
-	Q(k,i,j) = Q(k,i,j) -  dt/dy * ((nFy[k][j+1]  -  nFy[k][j]));
+        Q(k,i,j) = Q(k,i,j) -  dt/dy * ((nFy[k][j+1]  -  nFy[k][j]));
   }
 
 }
@@ -104,32 +114,55 @@ void laxf_scheme_2d(double *Q, double **ffx, double **ffy, double **nFx, double 
   But don't forget the subroutine laxf_scheme_2d
 */
 void solver(double *Q, double **ffx, double **ffy, double **nFx, double **nFy,
-	    int m, int n, double tend, double dx, double dy, double dt) {
+      int m, int n, double tend, double dx, double dy, double dt) {
   double bc_mask[3] = {1.0, -1.0, -1.0};
   double time;
   int i, j, k, steps;
   
-  steps = ceil(tend / dt);  
-  for (i = 0, time = 0.0; i < steps; i++, time += dt) { 
-
-    /* Apply boundary condition */
-    for (j = 1; j < n - 1 ; j++) {
-      for (k = 0; k < cell_size; k++) {
-	Q(k, 0, j) = bc_mask[k] *  Q(k, 1, j);
-	Q(k, m-1, j) = bc_mask[k] *  Q(k, m-2, j);
-      }
-    }
-
-    for (j = 0; j < m; j++)  {
-      for (k = 0; k < cell_size; k++) {
-	Q(k, j, 0) = bc_mask[k] * Q(k, j, 1);
-	Q(k, j, n-1) = bc_mask[k] * Q(k, j, n-2);
-      }
-    }
-     
-    /* Update all volumes with the Lax-Friedrich's scheme */     
-    laxf_scheme_2d(Q, ffx, ffy, nFx, nFy, m, n, dx, dy, dt);  
+  steps = ceil(tend / dt);
   
+  #pragma omp parallel private(ffx, ffy, nFx, nFy)
+  {
+    /* Allocate memory for fluxes */
+    ffx = (double **) malloc(cell_size * sizeof(double *));
+    ffy = (double **) malloc(cell_size * sizeof(double *));
+    nFx = (double **) malloc(cell_size * sizeof(double *));
+    nFy = (double **) malloc(cell_size * sizeof(double *));
+
+    ffx[0] = (double *) malloc(cell_size * m * sizeof(double));
+    ffy[0] = (double *) malloc(cell_size * n * sizeof(double));
+    nFx[0] = (double *) malloc(cell_size * m * sizeof(double));
+    nFy[0] = (double *) malloc(cell_size * n * sizeof(double));
+
+    for (i = 0; i < cell_size; i++) {
+      ffx[i] =  ffx[0] + i * m;
+      nFx[i] =  nFx[0] + i * m;
+      ffy[i] =  ffy[0] + i * n;
+      nFy[i] =  nFy[0] + i * n;
+    }
+
+    for (i = 0, time = 0.0; i < steps; i++, time += dt) { 
+      
+      /* Apply boundary condition */
+      #pragma omp for collapse(2)
+      for (j = 1; j < n - 1 ; j++) {
+        for (k = 0; k < cell_size; k++) {
+          Q(k, 0, j) = bc_mask[k] *  Q(k, 1, j);
+          Q(k, m-1, j) = bc_mask[k] *  Q(k, m-2, j);
+        }
+      }
+
+      #pragma omp for collapse(2)
+      for (j = 0; j < m; j++)  {
+        for (k = 0; k < cell_size; k++) {
+          Q(k, j, 0) = bc_mask[k] * Q(k, j, 1);
+          Q(k, j, n-1) = bc_mask[k] * Q(k, j, n-2);
+        }
+      }
+
+      /* Update all volumes with the Lax-Friedrich's scheme */     
+      laxf_scheme_2d(Q, ffx, ffy, nFx, nFy, m, n, dx, dy, dt);  
+    }
   }
 }
   
@@ -178,29 +211,29 @@ int main(int argc, char **argv) {
   Q = (double *) malloc(m * n * cell_size *  sizeof(double));
 
   x = (double *) malloc(m * sizeof(double));
-  y = (double *) malloc(n * sizeof(double));	
+  y = (double *) malloc(n * sizeof(double));  
 
-  /* Allocate memory for fluxes */
-  ffx = (double **) malloc(cell_size * sizeof(double *));
-  ffy = (double **) malloc(cell_size * sizeof(double *));
-  nFx = (double **) malloc(cell_size * sizeof(double *));
-  nFy = (double **) malloc(cell_size * sizeof(double *));
+  // /* Allocate memory for fluxes */
+  // ffx = (double **) malloc(cell_size * sizeof(double *));
+  // ffy = (double **) malloc(cell_size * sizeof(double *));
+  // nFx = (double **) malloc(cell_size * sizeof(double *));
+  // nFy = (double **) malloc(cell_size * sizeof(double *));
 
-  ffx[0] = (double *) malloc(cell_size * m * sizeof(double));
-  ffy[0] = (double *) malloc(cell_size * n * sizeof(double));
-  nFx[0] = (double *) malloc(cell_size * m * sizeof(double));
-  nFy[0] = (double *) malloc(cell_size * n * sizeof(double));
+  // ffx[0] = (double *) malloc(cell_size * m * sizeof(double));
+  // ffy[0] = (double *) malloc(cell_size * n * sizeof(double));
+  // nFx[0] = (double *) malloc(cell_size * m * sizeof(double));
+  // nFy[0] = (double *) malloc(cell_size * n * sizeof(double));
 
-  for (i = 0; i < cell_size; i++) {
-    ffx[i] =  ffx[0] + i * m;
-    nFx[i] =  nFx[0] + i * m;
-    ffy[i] =  ffy[0] + i * n;
-    nFy[i] =  nFy[0] + i * n;
-  }
-    
+  // for (i = 0; i < cell_size; i++) {
+  //   ffx[i] =  ffx[0] + i * m;
+  //   nFx[i] =  nFx[0] + i * m;
+  //   ffy[i] =  ffy[0] + i * n;
+  //   nFy[i] =  nFy[0] + i * n;
+  // }
+
   for (i = 0,tmp= -dx/2 + xstart; i < m; i++, tmp += dx)
     x[i] = tmp;
-
+  
   for (i = 0,tmp= -dy/2 + ystart; i < n; i++, tmp += dy)
     y[i] = tmp;
 
@@ -212,11 +245,11 @@ int main(int argc, char **argv) {
       Q(2, i, j) = 0.0;
     }
   }
-
+  
   for (i = 1; i < m-1; i++) {
     for (j = 1; j < n-1; j++) {
       Q(0, i, j) = 4.0 + epsi * exp(-(pow(x[i] - xend / 4.0, 2) + pow(y[j] - yend / 4.0, 2)) /
-					  (pow(delta, 2)));
+            (pow(delta, 2)));
     }
   }
 
@@ -228,10 +261,8 @@ int main(int argc, char **argv) {
 
   printf("Solver took %g seconds\n", etime - stime);
 
-
   /* Uncomment this line if you want visualize the result in ParaView */
-  /* save_vtk(Q, x, y, m, n); */
-
+  // save_vtk(Q, x, y, m, n);
 
   free(Q);
   free(x);
@@ -249,6 +280,7 @@ int main(int argc, char **argv) {
 
   return 0;    
 }
+
 
 
 
